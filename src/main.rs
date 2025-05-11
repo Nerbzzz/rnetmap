5c5c9ca10a0611411b6ab72e78a61b19001634a5
Nerbzzz <briandulkenraad2004@gmail.com>
2025-05-12 00:26:56 +0200

    Add basic CLI structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name = "rnetmap")]
+#[command(about = "Network Mapper designed with simplicity and performance in mind", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command(short_flag = 'p')]
+    Port {
+        #[arg(value_name = "PORTS", help = "22,80,1000-1010")]
+        ports: String,
+
+        #[arg(value_name = "HOST", help = "192.168.2.65/google.com")]
+        target: String,
+    },
+}
+
+
+#[tokio::main]
+async fn main() {
+    let args = Cli::parse();
+    match args.command {
+        Commands::Port { ports: _, target: _ } => {
+
+        }
+    }
 }
