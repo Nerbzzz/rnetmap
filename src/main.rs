1caa30e12b92f06b6925241a611b4ca16b88cdf9
Nerbzzz <briandulkenraad2004@gmail.com>
2025-05-12 00:56:40 +0200

    Add port scanning

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use clap::{Parser, Subcommand};
+use tokio::sync::mpsc;
+use util::parse_list;
+
+mod port;
+mod util;
 
 #[derive(Debug, Parser)]
 #[command(name = "rnetmap")]
@@ -20,13 +25,35 @@ enum Commands {
     },
 }
 
-
 #[tokio::main]
 async fn main() {
     let args = Cli::parse();
     match args.command {
-        Commands::Port { ports: _, target: _ } => {
+        Commands::Port { ports, target } => {
+    
+            let parsed_ports = parse_list(&ports).expect("Failed to parse list");
+            let (tx, mut rx) = mpsc::channel::<u16>(100);
+            for chunk in parsed_ports.chunks(50) {
+                let chunk_vec = chunk.to_vec();
+                let tx_clone = tx.clone();
+                let addr_clone = target.clone();
 
+                tokio::spawn(async move {
+                    for port in chunk_vec {
+                        port::scan_port(tx_clone.clone(), &addr_clone, port).await;
+                    }
+                });
+            }
+            drop(tx);
+
+            let mut open_ports = Vec::new();
+            while let Some(port) = rx.recv().await {
+                open_ports.push(port);
+            }
+            open_ports.sort_unstable();
+            for port in open_ports {
+                println!("Port {} is open", port);
+            }
         }
     }
 }
